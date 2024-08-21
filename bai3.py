3f9b62913c3b1106e5999786e13c2f5436045ddd
NguyenHongPhiYen <nguyenhongphiyen29@gmail.com>
2024-08-21 19:48:59 +0700

    Add files via upload

---
+++ bai3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 19:25:45 2024
+
+@author: Phiyen
+"""
+import math
+N=int(input("Nhập số nguyên dương có 2 chữ số:"))
+if 10<=N<=99:
+ print("Tổng của 2 chữ số nguyên dương:")    
+ Tong=(N//10)+(N%10)
+print("Tổng của N là", Tong)    
