859d0bf7e7b06e149b246397013d6ac810e95f29
NguyenHongPhiYen <nguyenhongphiyen29@gmail.com>
2024-08-21 19:58:38 +0700

    Add files via upload

---
+++ bai4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 19:48:32 2024
+
+@author: Student
+"""
+
+import math
+hours=int(input("Nhập giờ:"))
+minutes=int(input("Nhập phút:"))
+second=int(input("Nhập giây:"))
+Tong_doi_giay= hours*3600+ minutes*60+second
+print("Tổng giây là", Tong_doi_giay)
