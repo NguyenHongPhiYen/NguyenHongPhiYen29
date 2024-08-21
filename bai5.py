eaa1700fa8e96181efcbbadfbaa40bd55be6e244
NguyenHongPhiYen <nguyenhongphiyen29@gmail.com>
2024-08-21 20:06:48 +0700

    Add files via upload

---
+++ bai5.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 19:57:59 2024
+
+@author: Student
+"""
+
+import math
+namsinh=int(input("Nhập năm sinh của bạn:"))
+namhientai=int(input("Nhập năm hiện tại:"))
+sotuoi= namhientai - namsinh
+print("Số tuổi của bạn là:", sotuoi)
