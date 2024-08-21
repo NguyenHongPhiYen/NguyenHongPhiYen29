1f95a776e9f65b35562e2d6c13ff213adbb60558
NguyenHongPhiYen <nguyenhongphiyen29@gmail.com>
2024-08-21 19:25:24 +0700

    Add files via upload

---
+++ bai1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+var1=10
+print("Giá trị var1=", var1, "/n/t Kiểu:", type(var1))
+import math
+a=float(input("nhập số a:"))
+b=float(input("nhập số b:"))
+c=float(input("nhập số c:"))
+d=float(input("nhập số d:"))
+trung_bình= (a+b+c+d)/4
+print("Trung bình cộng của 4 số là", trung_bình)
+
+import math
+a=int(input("nhập số a:"))
+b=int(input("nhập số b:"))
+print("Tổng là a+b=", a+b)
+print("Hiệu là a-b= ", a-b)
+print("Tích là a*b=", a*b)
+print("Thương là a/b=", a/b)
+print("Chia lấy dư là a%b=", a%b)
+print("Chia lấy nguyên là a//b", a//b)
---
+++ bai2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 19:18:41 2024
+
+@author: Student
+"""
+
+import math
+a=int(input("nhập số a:"))
+b=int(input("nhập số b:"))
+print("Tổng là a+b=", a+b)
+print("Hiệu là a-b= ", a-b)
+print("Tích là a*b=", a*b)
+print("Thương là a/b=", a/b)
+print("Chia lấy dư là a%b=", a%b)
+print("Chia lấy nguyên là a//b", a//b)
