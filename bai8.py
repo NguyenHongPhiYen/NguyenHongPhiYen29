eb898f99d552be45506f17d2fe82ad6d80460299
NguyenHongPhiYen <nguyenhongphiyen29@gmail.com>
2024-08-22 13:43:05 +0700

    Add files via upload

---
+++ bai8.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 12:38:21 2024
+
+@author: Admin
+"""
+
+import math
+print("A=(32)**0.2-(1/64)**(-0.25)+(8/27)**(1/3)")
+A=(32)**0.2-(1/64)**(-0.25)+(8/27)**(1/3)
+ketqua=round(A,3)
+print("Kết quả là", ketqua)
