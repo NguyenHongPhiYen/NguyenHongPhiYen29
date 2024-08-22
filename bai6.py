ccb86e5703a1e825817669ccaff3eea8b7abf2d6
NguyenHongPhiYen <nguyenhongphiyen29@gmail.com>
2024-08-22 10:27:26 +0700

    Add files via upload

---
+++ bai6.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 10:19:21 2024
+
+@author: Admin
+"""
+
+import math
+a=int(input("nhập số a:"))
+b=int(input("nhập số b:"))
+c=int(input("nhập số c:"))
+print(f"{a}X^2+{b}X+{c}=0")
